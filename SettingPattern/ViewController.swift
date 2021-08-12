84f9b88bb22e9854ff23bf746574b2419b4b32a6
Roger Chang <d9352676@gmail.com>
2021-08-12 09:35:10 +0800

    Initial Commit

---
+++ SettingPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingPattern
+//
+//  Created by 張又壬 on 2021/8/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
